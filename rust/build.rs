5a509c9d4887a76b358fa0447583b92528ab3c8b
Tsukasa Arima <pyar6329@gmail.com>
2021-05-08 08:59:11 +0000

    add descriptor

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,10 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap_or("".to_string()));
     tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("chat_descriptor.bin"))
         .compile(&["../proto/chat.proto"], &["../proto"])
         .unwrap();
 }
